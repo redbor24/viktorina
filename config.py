692eb0d2f754cfd37f597d7de76207073aedcd08
redbor24 <redbor24@gmail.com>
2022-11-08 13:29:46 +0300

    Список состояний диалога перенесён в модуль `config`. Удалены неиспользуемые `import`-ы и переменные.

---
+++ config.py
@@ -0,0 +1,25 @@
+log_format = '%(asctime)s %(name)s:%(levelname)s:%(message)s'
+log_date_format = '%Y-%m-%d %H:%M:%S'
+vk_log_filename='victorina_vk.log'
+
+HELPME = 'ПАМАГИТИИИ!!!!'
+
+YES = 'Да'
+NO = 'Нет'
+GOGOGO = 'Поехали!!!'
+BYE = 'Ок, до свидания!'
+
+START_GAME = 'Привет, {0}!\nХочешь поучаствовать в викторине?'
+STOP_GAME = 'Больше вопросов нет.\nВы правильно ответили на {0} вопросов из {1}\nХотите сыграть ещё раз?'
+
+RIGHT_ANSWER = 'Правильный ответ:\n{0}'
+WRONG_ANSWER = 'Неправильный ответ... Попробуешь ещё раз ответить?'
+
+QUESTION = 'Вопрос:\n{0}'
+PRAISE = 'Правильно!'
+
+LET_NEW_GAME = 'Ну, что ж... Может, сыграем в новую игру?'
+LET_ANOTHER_TIME = 'Ну, что ж... Заходи как-нибудь, поиграем...'
+
+
+CHOOSING, NEXT_QUESTION, REPEAT_QUESTION, CHECK_ANSWER, END_GAME, REPEAT_GAME, UNKNOWN = range(7)
